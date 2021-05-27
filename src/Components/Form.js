2e54c936b134824cfe37f6cfa50c0d76fd0fa563
Tamzeed Rahman <tamzeedrahman@gmail.com>
2021-05-27 12:03:03 -0400

    yolo commit

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,15 +1,19 @@
-import React from 'react';
+import React from "react";
+import "./Form.css";
 
 const Form = ({ handleSubmit, handleChange }) => {
-    return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <input onChange={handleChange} type='text' placeholder="leave a comment" />
-            </form>
-        </div>
-    )
-
-}
-
+  return (
+    <div className="formcomment">
+      <form onSubmit={handleSubmit}>
+        <input
+          onChange={handleChange}
+          type="text"
+          placeholder="leave a comment"
+        />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
 
 export default Form;
--- src/Components/Video.js
+++ src/Components/Video.js
@@ -1,62 +1,66 @@
 import React from "react";
 import ReactPlayer from "react-player/youtube";
-import { v4 as uuid } from 'uuid';
-import Comment from './Comment';
-import Form from './Form';
+import { v4 as uuid } from "uuid";
+import Comment from "./Comment";
+import Form from "./Form";
 import "./Video.css";
 class Video extends React.Component {
   state = {
-    comment: '',
-    comments: []
-  }
+    comment: "",
+    comments: [],
+  };
 
   handleChange = (e) => {
     this.setState({
-      comment: e.target.value
-    })
-  }
+      comment: e.target.value,
+    });
+  };
 
   handleSubmit = (e) => {
-    e.preventDefault()
+    e.preventDefault();
     const newComment = {
       id: uuid(),
-      text: this.state.comment
-    }
+      text: this.state.comment,
+    };
     this.setState({
-      comments: [...this.state.comments, newComment]
-    })
-  }
-
+      comments: [...this.state.comments, newComment],
+    });
+  };
 
   remove = (id) => {
     this.setState({
-      comments: this.state.comments.filter((newComment) => newComment.id !== id)
-    })
-  }
-
+      comments: this.state.comments.filter(
+        (newComment) => newComment.id !== id
+      ),
+    });
+  };
 
   render() {
-    const { comments } = this.state
-    const listItem = comments.map(comment => {
-      return <Comment comment={comment} remove={this.remove} key={comment.id} text={comment.text} />
-    })
-    const { id } = this.props.match.params
+    const { comments } = this.state;
+    const listItem = comments.map((comment) => {
+      return (
+        <Comment
+          comment={comment}
+          remove={this.remove}
+          key={comment.id}
+          text={comment.text}
+        />
+      );
+    });
+    const { id } = this.props.match.params;
     return (
       <div>
         <div className="onevideo">
-          <ReactPlayer
-            url={`https://www.youtube.com/watch?v=${id}`}
-          />
+          <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`} />
         </div>
-        <Form handleChange={this.handleChange} handleSubmit={this.handleSubmit} />
-        <ul>
-          {listItem}
-        </ul>
+        <Form
+          handleChange={this.handleChange}
+          handleSubmit={this.handleSubmit}
+          className="Comment"
+        />
+        <ul>{listItem}</ul>
       </div>
-
-
     );
-
   }
 }
 
