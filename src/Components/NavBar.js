736490fcc26b2c58528d4a718e1f9f99b545ba7e
Talia Pichardo <taliapichardo@gmail.com>
2021-12-15 23:35:17 -0500

    Makes YouTube Logo the home button

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,22 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import YouTube_Logo from '../Assets/YouTube_Logo.svg'
 import "./NavBar.css";
 
 const NavBar = () => {
   return (
     <nav>
-      <img
-        src="https://upload.wikimedia.org/wikipedia/commons/b/b8/YouTube_Logo_2017.svg"
-        alt="youtube icon"
-        style={{ height: "100px", width: "200px" }}
-      />
-
-      <Link className="button blue" to="/">
-        Home
-        </Link>{" "}
+      <Link to='/'>
+        <img
+          src={YouTube_Logo}
+          alt="youtube icon"
+          style={{ height: "100px", width: "200px" }}
+        />
+      </Link>
       <Link className="button blue" to="/about">
         About
-        </Link>
+      </Link>
     </nav>
 
   );
